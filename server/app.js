d6590974d9746ea1319d9f9dc84cb1b9577b631e
sadegh hayeri <hayerisadegh@gmail.com>
2016-09-16 18:05:05 +0430

    socket.io add + ejs

--- server/app.js
+++ server/app.js
@@ -1,59 +1,49 @@
-var express = require('express');
 var path = require('path');
-var favicon = require('static-favicon');
-var logger = require('morgan');
+var DEBUG = require('debug')('app');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+const port = process.env.PORT || 3000;
 
+var express = require("express");
+var http = require("http");
 var app = express();
+var server = http.createServer(app).listen(port, function () {
+  console.log(`Twitter app listening on port ${port}!`);
+});
+var io = require("socket.io")(server);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
-app.use(favicon());
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
-
-/// catch 404 and forwarding to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+// Handle login form
+app.post('/new', function(req, res) {
+    console.log(req.body.url);
+    res.send('ok!');
 });
 
-/// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+// Handle 404
+app.use(function(req, res) {
+    DEBUG('404');
+    res.send('404: Page not Found', 404);
 });
 
+// Handle 500
+app.use(function(error, req, res, next) {
+    DEBUG('500');
+    res.send('500: Internal Server Error', 500);
+});
 
-module.exports = app;
+io.on('connection', function (socket) {
+    console.log('new connection!');
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+        console.log(data);
+    });
+});
