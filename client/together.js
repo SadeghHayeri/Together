ce7c849a3582ded08bd3068d44559e2bb144df62
SadeghHayeri <HayeriSadegh@gmail.com>
2016-12-20 20:31:00 +0330

    now socket work!

--- client/together.js
+++ client/together.js
@@ -1,33 +1,28 @@
 'use strict';
-var Downloader = require('downloader')
+var Downloader = require('./downloader.js')
 var request = require('request');
 var io = require('socket.io-client');
 
-class Together {
+function checkDownload(socket) {
+  socket.emit('imReady');
+}
 
-  constructor( serverInfo ) {
+function connect( serverInfo ) {
 
-    var socket = io.connect(`${serverInfo.ip}:${serverInfo.port}`, {reconnect: true});
-    socket.on('connect', function(socket) {
-      console.log('ok!');
-      Together.info.connect = true;
+  var socket = io.connect(`${serverInfo.ip}:${serverInfo.port}`, {reconnect: true});
+  socket.on('connect', function(socket) {
+
+    this.on('newChunk', (chunk) => {
+      var folderName = chunk.url.split("/")[chunk.url.split("/").length-1];
+      var currDownload = new Downloader( chunk.url, chunk.startRange, chunk.endRange, folderName, chunk.partNum, () => {
+        checkDownload(this);
+      });
+      currDownload.start();
     });
+    checkDownload(this);
 
-    // check status and
-    setTimeout(Together.imReady, 1000);
-
-  }
-
-  imReady() {
-    
-  }
+  });
 
 }
-Together.info = {
-  connect: false
-};
 
-var t = new Together({
-  ip: 'http://localhost',
-  port: 8080
-});
+connect({ ip: 'http://localhost', port: 8080 });
