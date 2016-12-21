71fca97c77888af106263b850d1bc45e7b0eef3c
SadeghHayeri <HayeriSadegh@gmail.com>
2016-12-21 19:14:37 +0330

    transmitter bugs fix! ^_^

--- client/together.js
+++ client/together.js
@@ -2,6 +2,10 @@
 var Downloader = require('./downloader.js')
 var request = require('request');
 var io = require('socket.io-client');
+var Transmitter = require('./transmitterClient.js');
+
+var downloads = [];
+var transmitter = {};
 
 function checkDownload(socket) {
   socket.emit('imReady');
@@ -11,18 +15,31 @@ function connect( serverInfo ) {
 
   var socket = io.connect(`${serverInfo.ip}:${serverInfo.port}`, {reconnect: true});
   socket.on('connect', function(socket) {
-
     this.on('newChunk', (chunk) => {
+
       var folderName = chunk.url.split("/")[chunk.url.split("/").length-1];
-      var currDownload = new Downloader( chunk.url, chunk.startRange, chunk.endRange, folderName, chunk.partNum, () => {
+      var newDownload = {
+        name: folderName,
+        partNum: chunk.partNum,
+        complete: false,
+      }
+      downloads.push(newDownload);
+
+      new Downloader( chunk.url, chunk.startRange, chunk.endRange, folderName, chunk.partNum, () => {
+        downloads[downloads.length-1].complete = true;
+        transmitter.sendFile(folderName, chunk.partNum);
         checkDownload(this);
-      });
-      currDownload.start();
+      }).start();
+
     });
     checkDownload(this);
-
   });
 
 }
 
-connect({ ip: 'http://localhost', port: 8080 });
+function start( ip, socketPort, TransmitterPort ) {
+  connect({ ip: ip, port: socketPort });
+  transmitter = new Transmitter({ ip: ip, port: TransmitterPort });
+}
+
+start( 'http://localhost', 8888, 9999);
