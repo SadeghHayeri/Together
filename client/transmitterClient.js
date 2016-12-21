84460ff1574c9a0abbd87f4f3e828854e95a0159
SadeghHayeri <HayeriSadegh@gmail.com>
2016-12-21 19:10:07 +0330

    rename transmitter

--- client/transmitterClient.js
+++ client/transmitterClient.js
@@ -13,12 +13,17 @@ class TransmitterClient {
   sendFile( fileName, partNum ) {
     var stream = ss.createStream();
     ss(TransmitterClient.socket).emit('sendFile', stream, {fileName: fileName, partNum: partNum});
-    fs.createReadStream(`Downloads/${fileName}/${fileName}.part${partNum}`).pipe(stream);
+    var f = fs.createReadStream(`Downloads/${fileName}/${fileName}.part${partNum}`).pipe(stream);
+    f.on('finish',() => {
+      fs.unlink(`Downloads/${fileName}/${fileName}.part${partNum}`, (err) => {
+        if (err) throw err;
+        console.log(`${fileName}/${fileName}.part${partNum} successfully deleted!`);
+      });
+      console.log('send finish!');
+    });
   }
 
 }
 TransmitterClient.socket = {};
 
-var t = new TransmitterClient({ ip: 'http://localhost', port: 9090 });
-t.sendFile( 'stunning-full-hd.jpeg', 0 );
-t.sendFile( 'googlelogo_color_272x92dp.png?v=3.5', 0 );
+module.exports = TransmitterClient;
