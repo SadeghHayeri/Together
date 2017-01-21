16fc671b0a3dbcafd89f89f841c403c05b66f6af
SadeghHayeri <hayerisadegh@gmail.com>
2017-01-21 17:29:03 +0330

    rename config.json to config.js

--- src/config.js
+++ src/config.js
@@ -1,8 +1,10 @@
-{
+var config = {
   "connection": {
     "socketPort": 4444,
     "transmitterPort": 5555,
     "bonjourPort": 6666
   },
-  "chunkSize": 1048576
+  "chunkSize": 10485760
 }
+
+module.exports = config
